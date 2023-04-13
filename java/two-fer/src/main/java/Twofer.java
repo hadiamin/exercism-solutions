3c06e41c960cdc39179a4366b19dec4af5feea97
Amin <aminhadi65@gmail.com>
2023-04-13 12:38:11 +0000

    Twofer - ternary approach improved

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,5 +1,5 @@
 public class Twofer {
     public String twofer(String name) {
-        return name == null ? "One for you, one for me." : "One for " + name + ", one for me";
+        return "One for " + (name == null ? "you" : name) + ", one for me.";
     }
 }
