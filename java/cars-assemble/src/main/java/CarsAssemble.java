318dcbf9249c155552e9c3e4711716d9db575689
Amin <aminhadi65@gmail.com>
2023-05-24 13:23:42 +0000

    CarsAssemble completed

---
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -0,0 +1,18 @@
+public class CarsAssemble {
+     int numberOfCars = 221;
+    public double productionRatePerHour(int speed) {
+        if (speed >= 1 && speed <= 4) {
+            return numberOfCars * speed;
+        } else if (speed >= 5 && speed <= 8) {
+            return numberOfCars * speed * 0.9;
+        } else if (speed == 9) {
+            return numberOfCars * speed * 0.8;
+        } else {
+            return numberOfCars * speed * 0.77;
+        }
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        return (int) (productionRatePerHour(speed) / 60);
+    }
+}
