1bd4d4b966d2ad7c1da923aca69888ba8feedea5
Amin <amin.hadi@turntabl.io>
2024-02-01 14:22:53 +0000

    isLeapYear - second iteration

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,9 +1,7 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        if ((year % 4 == 0) & (year % 100 != 0))
-            return true;
-        return year % 400 == 0;
+        return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
     }
 
 }
